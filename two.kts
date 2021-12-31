dd6b997742eb634e00976edd87dfee7a3485fe9c
songzhw <songzhw2012@gmail.com>
2021-12-31 10:41:08 -0500

    chore: test6

--- two.kts
+++ two.kts
@@ -1,3 +1,11 @@
 println("two.kts")
 
 println("args = ${args.toList()}")
+
+val msg = args[0]
+println("$msg  ** ${msg.startsWith("chore")}")
+if (!msg.startsWith("feat") || !msg.startsWith("fix") ||
+    !msg.startsWith("chore")
+) {
+    throw RuntimeException("commit message is wrong")
+}
