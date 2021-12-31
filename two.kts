ecc86ef71213988b4458413108c2d8f7cfb42403
songzhw <songzhw2012@gmail.com>
2021-12-31 10:42:50 -0500

    feat: test7

--- two.kts
+++ two.kts
@@ -4,7 +4,7 @@ println("args = ${args.toList()}")
 
 val msg = args[0]
 println("$msg  ** ${msg.startsWith("chore")}")
-if (!msg.startsWith("feat") || !msg.startsWith("fix") ||
+if (!msg.startsWith("feat") && !msg.startsWith("fix") &&
     !msg.startsWith("chore")
 ) {
     throw RuntimeException("commit message is wrong")
